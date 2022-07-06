853a066f49ef3f931aa56374c61385706f5f787d
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2022-07-06 11:44:21 +0300

    Add custom error messages to form validation, multiple messages are rendered

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -79,84 +79,100 @@
           </form>
           <!-- Registration Form -->
           <vee-form v-show="tab === 'register'" :validation-schema="schema"
-            @submit="register">
+            @submit="register" :initial-values="userData">
             <!-- Name -->
             <div class="mb-3">
-               <label class="inline-block mb-2">
+               <label class="inline-block mb-2"></label>
                 Name
-                <vee-field type="text" name="name" 
-                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                    duration-500 focus:outline-none focus:border-black rounded"
-                  placeholder="Enter Name" />
-                  <ErrorMessage class="text-red-600" name="name" />
-                </label>
+                <vee-field name="name" :bails="false" v-slot=" { field, errors }">
+                  <input type="text" class="block w-full py-1.5 px-3 text-gray-800
+                   border border-gray-300 transition
+                   duration-500 focus:outline-none focus:border-black rounded"
+                   placeholder="Enter Name" v-bind="field"/>
+                   <div class="text-red-600" v-for="error in errors" :key="error">
+                    {{ error }}
+                   </div>
+                </vee-field>
             </div>
             <!-- Email -->
             <div class="mb-3">
-              <label class="inline-block mb-2">
+              <label class="inline-block mb-2"></label>
                 Email
-                <vee-field name="email" type="email" 
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email" />
-                <ErrorMessage class="text-red-600" name="email" />
-              </label>
+                <vee-field name="email" :bails="false" v-slot="{ field, errors }">
+                  <input type="email"
+                    class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+                      duration-500 focus:outline-none focus:border-black rounded"
+                    placeholder="Enter Email" v-bind="field">
+                    <div class="text-red-600" v-for="error in errors" :key="error">
+                      {{ error }}
+                    </div>
+                  </vee-field>
             </div>
             <!-- Age -->
             <div class="mb-3">
-              <label class="inline-block mb-2">
+              <label class="inline-block mb-2"></label>
                 Age
-                <vee-field type="number"  name="age"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded" />
-                  <ErrorMessage class="text-red-600" name="age" />
-              </label>
+                <vee-field :bails="false" name="age" v-slot="{ errors, field }">
+                  <input type="number" v-bind="field"
+                   class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+                   duration-500 focus:outline-none focus:border-black rounded">
+                   <div class="text-red-600" v-for="error in errors">
+                    {{ error }}
+                   </div>
+                </vee-field>
             </div>
             <!-- Password -->
             <div class="mb-3">
-              <label class="inline-block mb-2">
-                Password
-                <vee-field type="password" name="password" 
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+              <label class="inline-block mb-2">Password</label>
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input type="password" class="block w-full py-1.5 px-3 text-gray-800
+                  border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" />
-                <ErrorMessage class="text-red-600" name="password" />
-                </label>
+                  placeholder="Password" v-bind="field" />
+                  <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
-              <label class="inline-block mb-2">
+              <label class="inline-block mb-2"></label>
                 Confirm Password
-                <vee-field type="password" name="confirm_password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Confirm Password" />
-                <ErrorMessage class="text-red-600" name="confirm_password" />
-                </label>
+                <vee-field name="confirm_password" :bails="false" v-slot="{ field, errors }">
+                <input type="password" class="block w-full py-1.5 px-3 text-gray-800
+                 border border-gray-300 transition
+                 first-letter:duration-500 focus:outline-none focus:border-black rounded"
+                placeholder="Confirm Password" v-bind="field">
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+                </vee-field>
             </div>
             <!-- Country -->
             <div class="mb-3">
-              <label class="inline-block mb-2">
+              <label class="inline-block mb-2"></label>
                 Country
-                <vee-field as="select" name="country"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded">
-                <option value="USA">USA</option>
-                <option value="Mexico">Mexico</option>
-                <option value="Germany">Germany</option>
-                <option value="Antarctica">Antarctica</option>
+                <vee-field name="country" :bails="false" v-slot="{ field, errors }">
+                  <select v-bind="field" class="block w-full py-1.5 px-3 text-gray-800 border
+                   border-gray-300 transition
+                    duration-500 focus:outline-none focus:border-black rounded">
+                     <option value="USA">USA</option>
+                      <option value="Mexico">Mexico</option>
+                      <option value="Germany">Germany</option>
+                      <option value="Antarctica">Antarctica</option>
+                  </select>
+                  <div class="text-red-600" v-for="error in errors" :key="error">
+                    {{ error }}
+                  </div>
                 </vee-field>
-                <ErrorMessage class="text-red-600" name="country" />
-              </label>
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
-              <label class="inline-block">
+              <label class="inline-block"></label>
                 <vee-field type="checkbox" name="tos" value="1"
                  class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
                 Accept terms of service
                 <ErrorMessage class="text-red-600" name="tos" />
-                </label>
             </div>
             <button type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
@@ -183,9 +199,12 @@ export default {
         email: 'required|min:3|max:100|email',
         age: 'required|minValue:18|maxValue:120',
         password: 'required|min:3|max:100',
-        confirm_password: 'confirmed:@password',
-        country: 'required|excluded:Antarctica',
-        tos: 'required',
+        confirm_password: 'passwords_mismatch:@password',
+        country: 'required|country_excluded:Antarctica',
+        tos: 'tos',
+      },
+      userData: {
+        country: 'USA',
       },
     };
   },
