3ec4d7b0f13dd2d3587ffe3e15beb8074ec57f89
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2022-07-06 11:44:21 +0300

    Add custom error messages to form validation

--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,6 +3,7 @@ import {
   Field as VeeField,
   defineRule,
   ErrorMessage,
+  configure,
 } from 'vee-validate';
 
 import {
@@ -24,13 +25,43 @@ export default {
     app.component('ErrorMessage', ErrorMessage);
 
     defineRule('required', required);
+    defineRule('tos', required);
     defineRule('min', min);
     defineRule('max', max);
     defineRule('alphaSpaces', alphaSpaces);
     defineRule('email', email);
     defineRule('minValue', minValue);
     defineRule('maxValue', maxValue);
-    defineRule('confirmed', confirmed);
+    defineRule('passwords_mismatch', confirmed);
     defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} is required`,
+          min: `The field ${ctx.field} is too short`,
+          max: `The field ${ctx.field} is too long`,
+          alphaSpaces: `The field ${ctx.field} may only contain alphabetical characters and spaces`,
+          email: `The field ${ctx.field} must be a valid email`,
+          minValue: `The field ${ctx.field} is too low`,
+          maxValue: `The field ${ctx.field} si too high`,
+          excluded: `You are not allowed to use this value for the field ${ctx.field}`,
+          country_excluded: 'Due to restrictions, we do not accept users from this location.',
+          passwords_mismatch: "The passwords don't match.",
+          tos: 'You must accept the terms of service.',
+        };
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid.`;
+        return message;
+      },
+
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
+    });
   },
 };
