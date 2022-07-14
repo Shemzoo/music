117af4f81b2ce9eabf88fa085a7c0257909bca03
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2022-07-14 12:02:10 +0300

    Add routing and manage page

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import AppHome from '@/views/AppHome.vue';
+import AppAbout from '@/views/AppAbout.vue';
+import AppManage from '@/views/AppManage.vue';
 
 const routes = [
   {
+    path: '/',
+    component: AppHome,
+    name: 'home',
+  },
+  {
     path: '/about',
+    component: AppAbout,
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+  },
+  {
+    path: '/manage-music',
+    component: AppManage,
+    name: 'manage',
+    // alias: '/manage',
+  },
+  {
+    path: '/manage',
+    redirect: { name: 'manage' },
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: { name: 'home' },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  linkExactActiveClass: 'text-yellow-500',
 });
 
 export default router;
