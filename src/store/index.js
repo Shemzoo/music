157a9e377ebfee9233a21d45d12a8bb5adf49511
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2022-07-27 11:03:02 +0300

    Add audio playing, play/pause button, progress bar

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,27 @@
 import { createStore } from 'vuex';
 import { auth, usersCollection } from '@/includes/firebase';
+import { Howl } from 'howler';
+import helper from '@/includes/helper';
 
 export default createStore({
   state: {
     authModalShow: false,
     userLoggedIn: false,
+    currentSong: {},
+    sound: {},
+    seek: '00:00',
+    duration: '00:00',
+    playerProgress: '0%',
   },
   getters: {
     // getAuthModalShow: (state) => state.authModalShow,
+    playing: (state) => {
+      if (state.sound.playing) {
+        return state.sound.playing();
+      }
+
+      return false;
+    },
   },
   mutations: {
     toggleAuthModal: (state) => {
@@ -15,6 +29,18 @@ export default createStore({
     },
     toggleAuth(state) {
       state.userLoggedIn = !state.userLoggedIn;
+    },
+    newSong(state, payload) {
+      state.currentSong = payload;
+      state.sound = new Howl({
+        src: [payload.url],
+        html5: true,
+      });
+    },
+    updatePosition(state) {
+      state.seek = helper.formatTime(state.sound.seek());
+      state.duration = helper.formatTime(state.sound.duration());
+      state.playerProgress = `${(state.sound.seek() / state.sound.duration()) * 100}%`;
     },
   },
   actions: {
@@ -58,6 +84,43 @@ export default createStore({
       // if (payload.route.meta.requiresAuth) {
       //   payload.router.push({ name: 'home' });
       // }
+    },
+
+    async newSong({ commit, state, dispatch }, payload) {
+      if (state.sound instanceof Howl) {
+        state.sound.unload();
+      }
+      commit('newSong', payload);
+
+      state.sound.play();
+
+      state.sound.on('play', () => {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      });
+    },
+
+    async toggleAudio({ state }) {
+      if (!state.sound.playing) {
+        return;
+      }
+
+      if (state.sound.playing()) {
+        state.sound.pause();
+      } else {
+        state.sound.play();
+      }
+    },
+
+    progress({ commit, state, dispatch }) {
+      commit('updatePosition');
+
+      if (state.sound.playing()) {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      }
     },
   },
   modules: {
