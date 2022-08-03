2d91992588390b479d560e9a3f3837948f90b49e
Shemzoo <46892255+Shemzoo@users.noreply.github.com>
2022-08-03 10:42:43 +0300

    Add PWA, workbox cache

--- vue.config.js
+++ vue.config.js
@@ -15,4 +15,11 @@ module.exports = defineConfig({
       fullInstall: true,
     },
   },
+  pwa: {
+    name: 'Music App',
+    themeColor: '#ff5e3a',
+    manifestOptions: {
+      short_name: 'Music',
+    },
+  },
 });
